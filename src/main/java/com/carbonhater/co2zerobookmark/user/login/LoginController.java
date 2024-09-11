7ce4c08665a0148793870dc27bb4c8c3e9dc327f
JaeHyun <skwent77@gmail.com>
2024-09-11 16:18:36 +0900

    chore: 빌드 의존성 추가 및 제거
    build.gradle
    1. 추가: spring-security-test 미리 추가
    2. 삭제: API 보안 관련 라이브러리 제거

--- src/main/java/com/carbonhater/co2zerobookmark/user/login/LoginController.java
+++ src/main/java/com/carbonhater/co2zerobookmark/user/login/LoginController.java
@@ -1,10 +1,9 @@
 package com.carbonhater.co2zerobookmark.user.login;
 
-import com.carbonhater.co2zerobookmark.user.dto.LoginResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
