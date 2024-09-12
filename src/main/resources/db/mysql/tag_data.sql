f97c470d5933966fe5b8ce06b159db9d37055b01
hoya <sv05159@naver.com>
2024-09-12 09:26:45 +0900

    feat: 태그 조회하기 기능 구현
    - 태그 조회 API 개발
    - 태그 샘플데이터 쿼리 추가 (resources/db/mysql/tag_data.sql)
    
    Fixes: #3

---
+++ src/main/resources/db/mysql/tag_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO Tag (tag_name, tag_color_code, tag_description, deletedyn, created_by, created_id, modified_by, modified_id)
+VALUES
+    ('Urgent', '#FF5733', 'Tasks that need immediate attention', 'N', '2024-09-09 12:00:00', 0, '2024-09-09 12:00:00', 0),
+    ('Important', '#33FF57', 'High-priority tasks', 'N', '2024-09-09 12:00:00', 0, '2024-09-09 12:00:00', 0),
+    ('Optional', '#3357FF', 'Tasks that are not time-sensitive', 'N', '2024-09-09 12:00:00', 0, '2024-09-09 12:00:00', 0),
+    ('Completed', '#FF33A5', 'Tasks that have been finished', 'N', '2024-09-09 12:00:00', 0, '2024-09-09 12:00:00', 0);
