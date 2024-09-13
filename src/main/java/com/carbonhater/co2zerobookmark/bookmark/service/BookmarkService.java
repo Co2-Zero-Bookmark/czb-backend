08a8294fa88d6ea053f6ab56e427ec223e0b816d
Shanate <redeyes5240@gmail.com>
2024-09-13 10:06:08 +0900

    docs : 미사용 Import 제거 미사용 Import 제거 footer :

--- src/main/java/com/carbonhater/co2zerobookmark/bookmark/service/BookmarkService.java
+++ src/main/java/com/carbonhater/co2zerobookmark/bookmark/service/BookmarkService.java
@@ -5,7 +5,6 @@ import com.carbonhater.co2zerobookmark.bookmark.model.BookmarkUpdateDTO;
 import com.carbonhater.co2zerobookmark.bookmark.repository.entity.*;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
