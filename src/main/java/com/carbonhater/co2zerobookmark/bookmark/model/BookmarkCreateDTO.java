6110aa9a4c00c1181a968d5b3a1dd3a952707fcd
Shanate <redeyes5240@gmail.com>
2024-09-13 09:57:26 +0900

    docs : Import 제거 불필요 Import 제거 footer :

--- src/main/java/com/carbonhater/co2zerobookmark/bookmark/model/BookmarkCreateDTO.java
+++ src/main/java/com/carbonhater/co2zerobookmark/bookmark/model/BookmarkCreateDTO.java
@@ -1,11 +1,8 @@
 package com.carbonhater.co2zerobookmark.bookmark.model;
 
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
-import org.apache.logging.log4j.core.config.plugins.validation.constraints.NotBlank;
 
-import java.util.List;
 
 @Getter
 @Setter
--- src/main/java/com/carbonhater/co2zerobookmark/bookmark/model/BookmarkUpdateDTO.java
+++ src/main/java/com/carbonhater/co2zerobookmark/bookmark/model/BookmarkUpdateDTO.java
@@ -1,10 +1,8 @@
 package com.carbonhater.co2zerobookmark.bookmark.model;
 
 
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
-import org.apache.logging.log4j.core.config.plugins.validation.constraints.NotBlank;
 
 @Getter
 @Setter
