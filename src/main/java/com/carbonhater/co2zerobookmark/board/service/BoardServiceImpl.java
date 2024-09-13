ff726e40b201315e6db28bd7bee8d4ed617d0414
seokjin <tjrwls08088@naver.com>
2024-09-13 14:23:55 +0900

    fix: 공통 Response & Exception board, bookmark에 적용 board, bookmark (create만 적용) 도메인에 적용 boardServiceImpl에는 다음과 같은 NotFoundException 예외처리 진행
    footer:

--- src/main/java/com/carbonhater/co2zerobookmark/board/service/BoardServiceImpl.java
+++ src/main/java/com/carbonhater/co2zerobookmark/board/service/BoardServiceImpl.java
@@ -4,10 +4,13 @@ package com.carbonhater.co2zerobookmark.board.service;
 import com.carbonhater.co2zerobookmark.board.model.BoardResponseDTO;
 import com.carbonhater.co2zerobookmark.board.repository.BoardRepository;
 import com.carbonhater.co2zerobookmark.board.repository.LikeRepository;
+import com.carbonhater.co2zerobookmark.board.repository.entity.Board;
+import com.carbonhater.co2zerobookmark.common.exception.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,8 +24,15 @@ public class BoardServiceImpl implements BoardService{
 
     @Override
     public List<BoardResponseDTO> getAllBoards() {
+        List<Board> boards = boardRepository.findAllBoardsByDeletedYn('N');
 
-        return boardRepository.findAllBoardsByDeletedYn('N').stream()
+        // 리스트가 비어있는지 확인하고 예외 던지기
+        if (boards.isEmpty()) {
+            throw new NotFoundException("게시판이 존재하지 않습니다");
+        }
+
+        // 전체 보드 리스트를 변환
+        List<BoardResponseDTO> boardResponseDTOs = boards.stream()
                 .map(board -> {
                     long likeCount = likeRepository.countByBoardIdAndDeletedYn(board.getBoardId(), 'N');
                     return BoardResponseDTO.builder()
@@ -34,5 +44,7 @@ public class BoardServiceImpl implements BoardService{
                             .build();
                 })
                 .collect(Collectors.toList());
+
+        return boardResponseDTOs;
     }
 }
