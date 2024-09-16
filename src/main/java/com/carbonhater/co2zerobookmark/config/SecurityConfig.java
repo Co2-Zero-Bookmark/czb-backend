be3abb1a6d69a4bb7506b32259f193aa6fcf2a01
seokjin <tjrwls08088@naver.com>
2024-09-16 14:10:29 +0900

    fix: security config 수정 api 테스트 위해 url 설정
    footer:

--- src/main/java/com/carbonhater/co2zerobookmark/config/SecurityConfig.java
+++ src/main/java/com/carbonhater/co2zerobookmark/config/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig {
             .csrf(csrf -> csrf.disable())// CSRF 보호 비활성화 (REST 환경)
             .formLogin((auth) -> auth.disable())
                 .authorizeHttpRequests(authorize -> authorize
-                    .requestMatchers("/api/v1/users/signup", "api/v1/users/login", "/api/v1/*").permitAll()
+                    .requestMatchers("/api/v1/users/signup", "api/v1/users/login", "/api/v1/**").permitAll()
                     .anyRequest().authenticated())
                 .sessionManagement((session) ->
                     session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
