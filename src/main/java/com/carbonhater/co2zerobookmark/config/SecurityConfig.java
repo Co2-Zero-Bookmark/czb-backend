d4fe3777be2e6058aed56108904fd336adb26f0e
seokjin <tjrwls08088@naver.com>
2024-09-16 14:10:57 +0900

    doc: security config 수정 api 테스트 위해 url 설정
    footer:

--- src/main/java/com/carbonhater/co2zerobookmark/config/SecurityConfig.java
+++ src/main/java/com/carbonhater/co2zerobookmark/config/SecurityConfig.java
@@ -1,15 +1,9 @@
 package com.carbonhater.co2zerobookmark.config;
 
 
-import io.swagger.v3.oas.models.info.Info;
-import jakarta.servlet.Filter;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -17,11 +11,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder; // BCrypt 해시 함수로 암호화하는 구현체
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 //비밀번호 암호화 관련: PasswordEncoder의 구현체 대입 -> Bean 등록
 @Configuration
