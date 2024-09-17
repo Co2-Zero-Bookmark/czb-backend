dbd06945236a33491938792dfb812149bca70e83
seokjin <tjrwls08088@naver.com>
2024-09-18 01:35:51 +0900

    style: 안쓰는 import 구문 제거 footer:

--- src/main/java/com/carbonhater/co2zerobookmark/board/repository/LikeRepository.java
+++ src/main/java/com/carbonhater/co2zerobookmark/board/repository/LikeRepository.java
@@ -3,8 +3,6 @@ package com.carbonhater.co2zerobookmark.board.repository;
 
 import com.carbonhater.co2zerobookmark.board.repository.entity.Like;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
