d513a784521f1da814460ec231faed1460393853
hoya <sv05159@naver.com>
2024-09-17 23:29:23 +0900

    feat: 폴더 계층구조 조회하기 API
    - 폴더 계층구조 조회하기 API
    - 스프링 시큐리티 개발 필요
    
    Fixes: #23
    Related to: #7

--- src/main/java/com/carbonhater/co2zerobookmark/bookmark/controller/FolderController.java
+++ src/main/java/com/carbonhater/co2zerobookmark/bookmark/controller/FolderController.java
@@ -15,6 +15,12 @@ public class FolderController {
 
     private final FolderService folderService;
 
+    @GetMapping
+    public ResponseEntity<Object> getFolders() {
+        long userId = 0L; //TODO 스프링 시큐리티 개발 필요
+        return ResponseEntity.ok().body(folderService.getFolderHierarchyByUserId(userId));
+    }
+
     @PostMapping
     public ResponseEntity<Object> createFolders(@RequestBody FoldersCreateDto foldersCreateDto) {
         Long userId = 0L; //TODO 스프링 시큐리티 개발필요
