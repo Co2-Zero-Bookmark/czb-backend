5e1c91f5c78d6929fadfd703fbb2909b27c3f4ea
Shanate <redeyes5240@gmail.com>
2024-09-19 10:36:19 +0900

    fix: Merge Conflict 머지되지 않는 오류가 있어 해당 문제 수정

--- src/main/java/com/carbonhater/co2zerobookmark/bookmark/repository/entity/BookmarkRepository.java
+++ src/main/java/com/carbonhater/co2zerobookmark/bookmark/repository/entity/BookmarkRepository.java
@@ -1,11 +1,9 @@
 package com.carbonhater.co2zerobookmark.bookmark.repository.entity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long>
                                             , BookmarkRepositoryCustom {
     List<Bookmark> findByDeletedYn(char deletedYn);
