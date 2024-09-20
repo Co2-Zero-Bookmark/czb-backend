77ca6d346f2b53db604450f6f3e8048a5602e096
seokjin <tjrwls08088@naver.com>
2024-09-20 09:40:03 +0900

    fix: 인가 처리 진행 footer:

--- src/main/java/com/carbonhater/co2zerobookmark/board/controller/BoardController.java
+++ src/main/java/com/carbonhater/co2zerobookmark/board/controller/BoardController.java
@@ -2,10 +2,13 @@ package com.carbonhater.co2zerobookmark.board.controller;
 
 import com.carbonhater.co2zerobookmark.board.model.BoardResponseDTO;
 import com.carbonhater.co2zerobookmark.board.model.LikeRequestDTO;
+import com.carbonhater.co2zerobookmark.board.repository.entity.Like;
 import com.carbonhater.co2zerobookmark.board.service.BoardService;
 import com.carbonhater.co2zerobookmark.common.response.CustomResponseEntity;
+import com.carbonhater.co2zerobookmark.user.service.SignService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,21 +22,33 @@ import static com.carbonhater.co2zerobookmark.common.response.CustomResponseEnti
 public class BoardController {
 
     private final BoardService boardService;
+    private final SignService signService;
 
     @GetMapping("")
     public CustomResponseEntity<List<BoardResponseDTO>> getBoards(){
-            Long userId = 2L;
-            log.info("getBoards Controller");
+            String userEmail = SecurityContextHolder.getContext().getAuthentication().getName();
+            Long userId = signService.getUserIdByEmail(userEmail);
+            log.info("getBoards Controller ==== user Email: {}, userId: {}", userEmail, userId);
             return success(boardService.getAllBoards(userId));
     }
 
-    @PostMapping("/{boardId}/like")
-    public CustomResponseEntity<String> likeBoard(@PathVariable Long boardId, @RequestBody LikeRequestDTO likeRequestDTO){
-        likeRequestDTO.setBoardId(boardId);
-        log.info("likeBoard Controller : {}", likeRequestDTO);
-        return success(boardService.likeBoard(likeRequestDTO));
+    @PostMapping("/folder/{folderId}")
+    public CustomResponseEntity<BoardResponseDTO> createBoard(@PathVariable Long folderId){
+        String userEmail = SecurityContextHolder.getContext().getAuthentication().getName();
+        Long userId = signService.getUserIdByEmail(userEmail);
+        log.info("createBoard Controller ==== user Email: {}, userId: {}", userEmail, userId);
+        return success(boardService.createBoard(folderId, userId));
     }
 
+    @PostMapping("/{boardId}/like")
+    public CustomResponseEntity<String> likeBoard(@PathVariable Long boardId){
+        String userEmail = SecurityContextHolder.getContext().getAuthentication().getName();
+        Long userId = signService.getUserIdByEmail(userEmail);
+        LikeRequestDTO likeRequestDTO = new LikeRequestDTO(boardId, userId);
+        log.info("likeBoard Controller : {}, boardID: {}", userId, boardId);
+
+        return success(boardService.likeBoard(likeRequestDTO));
+    }
 
     @DeleteMapping("/{boardId}/like")
     public CustomResponseEntity<String> dislikeBoard(@PathVariable Long boardId, LikeRequestDTO likeRequestDTO){
